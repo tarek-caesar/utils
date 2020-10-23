8a726b30a2d1ec1e5e3aa18845ede5c3b6076c2b
Tarek, Ahmed (Intero Technologies) <a.tarek@intero-technologies.de>
2020-10-23 15:26:26 +0200

    Robot Framework installation script

---
+++ install_robot.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#################################################################################
+# Author : Ahmed Tarek
+# Description : Installing Robot Framework with the required tools
+#################################################################################
+
+apt-get -y update
+apt-get -y install wget
+
+echo "######### Installing Chrome Browser #########"
+apt-get -y install libxss1 libappindicator1 libindicator7
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+apt -y install ./google-chrome*.deb
+
+echo "######### Installing Python3 & pip3 #########"
+apt-get -y install python3
+apt-get -y install python3-pip
+
+
+echo "######### Installing Robot Framework #########"
+pip3 install robotframework
+export PATH="$HOME/.local/bin:$PATH"
+
+echo "######### Installing Selenium library for Robot Framework #########"
+pip3 install robotframework-selenium2library
+pip3 install -U selenium
+
+echo "######### Installing Chrome Driver ########"
+wget https://chromedriver.storage.googleapis.com/86.0.4240.22/chromedriver_linux64.zip
+unzip chromedriver_linux64.zip -d /usr/local/bin
