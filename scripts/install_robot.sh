5b3ab31b2c27e853d85de83c8d575f0794892792
Ahmed Tarek <atarekmokhtar90@gmail.com>
2021-07-01 14:17:57 +0200

    Update chrome driver version

--- scripts/install_robot.sh
+++ scripts/install_robot.sh
@@ -26,5 +26,5 @@ pip3 install robotframework-selenium2library
 pip3 install -U selenium
 
 echo "######### Installing Chrome Driver ########"
-wget https://chromedriver.storage.googleapis.com/91.0.4472.19/chromedriver_linux64.zip
+wget https://chromedriver.storage.googleapis.com/91.0.4472.101/chromedriver_linux64.zip
 unzip chromedriver_linux64.zip -d /usr/local/bin
